5edf7e531c3baa697b4e8f0c968cd0a1a8c92e4e
Bo Brinkman <brinkmwj@server1.hushsunhush.com>
2015-06-02 11:26:49 +0000

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ala_program
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject ala_program "0.1.0-SNAPSHOT"
+  :description "Web service to randomly generate ALA session names"
+  :url "http://bobrinkman.com:8080"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"] 
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-jetty-adapter "1.3.2"]])
