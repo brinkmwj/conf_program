3c820e73385d6d7e8dcc05c3b211c54307cd8127
Bo Brinkman <brinkmwj@server1.hushsunhush.com>
2015-06-02 13:29:24 +0000

    Working json response

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
 (defproject ala_program "0.1.0-SNAPSHOT"
   :description "Web service to randomly generate ALA session names"
-  :url "http://bobrinkman.com:8080"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "http://bobrinkman.com:3000"
+  :license {:name "GNU General Public License"
+            :url "https://github.com/brinkmwj/ala_program/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.6.0"] 
                  [ring/ring-core "1.3.2"]
-                 [ring/ring-jetty-adapter "1.3.2"]])
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [compojure "1.3.4"]
+                 [ring/ring-json "0.3.1"]
+                 [cheshire "5.5.0"]]
+  :plugins [[lein-ring "0.9.4"]]
+  :ring {:handler ala-program.core/app})
--- src/ala_program/core.clj
+++ src/ala_program/core.clj
@@ -1,6 +1,19 @@
-(ns ala-program.core)
+(ns ala-program.core 
+  (:use compojure.core)
+  (:use cheshire.core)
+  (:use ring.util.response)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [ring.middleware.json :as middleware]))
 
-(defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+(defn get-one-title []
+  (response '{:title "Panel title" :author "Author name"}))
+
+(defroutes app-routes
+  (GET "/" [] (get-one-title))
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (handler/api app-routes)
+      (middleware/wrap-json-body)
+      (middleware/wrap-json-response)))
