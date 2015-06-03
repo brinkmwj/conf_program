57a79b2c0c86502e166bd71e0ac5858e7ff681f2
Bo Brinkman <brinkmwj@miamioh.edu>
2015-06-03 18:16:19 +0000

    Fully functional title generation

--- src/ala_program/core.clj
+++ src/ala_program/core.clj
@@ -14,56 +14,73 @@
 (def file-lines
   (clojure.string/split-lines (slurp data-file)))
 
+;;Don't want any lines with less than 2 words because they can't generate useful data
 (def clean-lines
-  (map (fn [x] (clojure.string/replace x #"\s+" " ")) file-lines))
+  (filter (fn [x] (>= (count x) 2)) (map (fn [x] (clojure.string/replace x #"\s+" " ")) file-lines)))
 
 (def starters
-  (map (fn [x] (take 2 (clojure.string/split x #" "))) clean-lines))
+  (vec (map (fn [x] (take 2 (clojure.string/split x #" "))) clean-lines)))
+
+(def enders
+  (map (fn [x] (concat (take-last 2 (clojure.string/split x #" ")) (list nil))) clean-lines))
+
+(defn ender? [x]
+  (nil? (nth x 2)))
 
 (def trigram-list
-  (apply concat (map (fn [x] (partition 3 1 (clojure.string/split x #" "))) clean-lines)))
+  (concat enders (apply concat (map (fn [x] (partition 3 1 (clojure.string/split x #" "))) clean-lines))))
 
-(def bigram-counts
-  (frequencies (map (fn [x] (take 2 x)) trigram-list)))
+(defn get-two-and-lowercase [trigram]
+  (map clojure.string/lower-case (take 2 trigram)))
 
 (def trigram-lookup-list
-  (group-by (fn [x] (take 2 x)) trigram-list))
+  (group-by get-two-and-lowercase trigram-list))
 
+(defn get-random-starter []
+  (nth starters (rand-int (count starters))))
 
+(defn get-random-next-trigram [bigram]
+  (let [options (trigram-lookup-list (get-two-and-lowercase bigram))]
+    (nth options (rand-int (count options)))))
 
+(defn get-random-from [bigram]
+  (let [next-trigram (get-random-next-trigram bigram)]
+    (cons (first bigram) 
+          (if (ender? next-trigram)
+            (list (nth bigram 1))
+            (get-random-from (rest next-trigram))))))
 
+(defn get-random-panel []
+  (clojure.string/join " " (get-random-from (get-random-starter))))
 
-(defn get-corpus []
-  (group-by (fn [x] (take 2 (key x))) (frequencies (partition 3 1 (take 100000 (clojure.string/split (clojure.string/replace (slurp data-file) #"\s+" " ") #" "))))))
+(defn levenshtein [w1 w2]
+  (letfn [(cell-value [same-char? prev-row cur-row col-idx]
+            (min (inc (nth prev-row col-idx))
+                 (inc (last cur-row))
+                 (+ (nth prev-row (dec col-idx)) (if same-char?
+                                                   0
+                                                   1))))]
+    (loop [row-idx  1
+           max-rows (inc (count w2))
+           prev-row (range (inc (count w1)))]
+      (if (= row-idx max-rows)
+        (last prev-row)
+        (let [ch2           (nth w2 (dec row-idx))
+              next-prev-row (reduce (fn [cur-row i]
+                                      (let [same-char? (= (nth w1 (dec i)) ch2)]
+                                        (conj cur-row (cell-value same-char?
+                                                                  prev-row
+                                                                  cur-row
+                                                                  i))))
+                                    [row-idx] (range 1 (count prev-row)))]
+          (recur (inc row-idx) max-rows next-prev-row))))))
 
-(def the-corpus (get-corpus))
-
-(defn count-bigrams-for-key [k] 
-  (reduce + (map (fn [x] (nth x 1)) (the-corpus k))))
-
-(defn get-bigram [num coll]
-  (if (< num (nth (first coll) 1))
-    (first (first coll))
-    (get-bigram (- num (nth (first coll) 1)) (rest coll))))
-
-(defn get-bigram-for-key [key num] 
-  (get-bigram num (the-corpus key)))
-
-(defn get-random-next-word [word]
-  (let [count (count-bigrams-for-key word)]
-    (nth (get-bigram-for-key word (rand-int count)) 2)))
-
-(defn get-some-words [startword count]
-  (let [nextword (get-random-next-word startword)] 
-    (if (<= count 1) 
-      startword
-      (cons (first startword) (get-some-words (list (nth startword 1) nextword) (- count 1))))))
-
-(defn get-one-title []
-  (clojure.string/join " " (get-some-words (list "the" "wall") 15)))
+(defn min-lev-dist [string coll]
+  (reduce min (map (fn [x] (levenshtein string x)) coll)))
 
 (defn get-one-panel []
-  (response {:title bigram-counts :author "Author name"}))
+  (let [pname (get-random-panel)]
+    (response {:title pname :alreadyhere (some #(= pname %) file-lines)})))
 
 (defroutes app-routes
   (GET "/" [] "<h1>floop</h1>")
